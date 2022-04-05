0531f660bf426f1c126910dd60ae31fd4d26ca90
Jørgen Finsveen <joergen.finsveen@gmail.com>
2022-04-05 15:04:02 +0200

    fixed issue with module-info.java - maven.package now works properly

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,7 @@ module wargames {
     requires javafx.fxml;
 
     opens idatx2001.jorgfi.wargamesApp.controller to javafx.fxml;
-    opens idatx2001.jorgfi.wargamesApp.model to javafx.fxml;
+    opens idatx2001.jorgfi.wargamesApp.model;
 
     exports idatx2001.jorgfi.wargamesApp.controller;
     exports idatx2001.jorgfi.wargamesApp.model;
